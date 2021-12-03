2d6d6a73315726cfec66eaad10801c74884c0eb5
lugoz99 <lugoest99@gmail.com>
2021-12-03 00:47:46 -0500

    Modificando navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './public/template/header/header.component';
-import { FooterComponent } from './public/template/footer/footer.component';
 import { HomeComponent } from './public/general/home/home.component';
 import { NavbarComponent } from './public/template/navbar/navbar.component';
 import { NotFoundComponent } from './public/errors/not-found/not-found.component';
 import { InternalServerErrorComponent } from './public/errors/internal-server-error/internal-server-error.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     HomeComponent,
     NavbarComponent,
     NotFoundComponent,
     InternalServerErrorComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/assets/js/init-M.js
+++ src/assets/js/init-M.js
@@ -11,3 +11,17 @@ function InitSelect(selectId) {
   var elems = document.querySelector("#" + selectId);
   var instances = M.FormSelect.init(elems, {});
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".dropdown-trigger");
+  console.log("se hizo click");
+  var instances = M.Dropdown.init(elems, {});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elem = document.querySelector(".collapsible");
+  var instance = M.Collapsible.init(elem, {
+    accordion: true,
+    inDuration: 250,
+  });
+});
